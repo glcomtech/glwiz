f211eed2cd4179ef7ba3683bfc5b02758561a183
git-user-cpp <akushyk799@gmail.com>
2025-02-13 14:46:38 +0200

    Cargo.toml updated, print_license_info function fixed

--- src/prog_fun.rs
+++ src/prog_fun.rs
@@ -3,5 +3,5 @@ pub fn print_license_info() {
     println!("gnulinwiz AKA GNU/Linux Config Wizard  Copyright (C) 2025  Andrew Kushyk\n\
 This program comes with ABSOLUTELY NO WARRANTY; for details see https://www.gnu.org/licenses/gpl-3.0.html/\n\
 This is free software, and you are welcome to redistribute it\n\
-under certain conditions; for details see https://www.gnu.org/licenses/gpl-3.0.html/");
+under certain conditions; for details see https://www.gnu.org/licenses/gpl-3.0.html/\n");
 } // print_license_info
