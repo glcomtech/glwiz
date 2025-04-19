17a125ba37ed0a2ffed73c8eb6cc2ebc5c17e317
git-user-cpp <akushyk799@gmail.com>
2025-04-19 18:39:27 +0300

    added license info

--- src/functionality/env.rs
+++ src/functionality/env.rs
@@ -1,3 +1,23 @@
+/*
+gnulinwiz AKA GNU/Linux Config Wizard: The ultimate post-installation setup assistant for Linux,
+streamlining your configuration process with ease and precision.
+
+Copyright (C) 2025  Andrew Kushyk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use colored::Colorize;
 use std::env::var;
 
